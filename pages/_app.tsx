7218b33f5a4de14c60fffc23b29cfa1e5957f6a2
Sithum Sathsara <89584440+SithumSathsaras@users.noreply.github.com>
2024-08-21 00:27:12 +0530

    fix deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,7 @@ import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css'; // Import global styles
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+  return<Component {...pageProps} />
+     
+   
 }
